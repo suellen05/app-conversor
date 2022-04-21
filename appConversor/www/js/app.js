68d303a5ec1a893c1d80e4d7d618169ae3e78a51
Suellen <73621600+suellen05@users.noreply.github.com>
2022-04-20 22:03:06 -0300

    Conversor de Medidas
    Criar a funcionalidade de conversão de valores

---
+++ appConversor/www/js/app.js
@@ -0,0 +1,9 @@
+function ConversorMetrosCentimetros(){
+let metros = document.querySelector("#metros").value
+
+let centimetros = metros * 100
+document.querySelector("#centimetros").innerHTML = `${centimetros} cm`
+
+}
+
+console.warn("ola")
